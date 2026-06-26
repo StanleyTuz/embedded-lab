3b70810e0233c76231fbb2ed075f17fbfed18f4c
StanleyTuz <stanley.tuznik@gmail.com>
2026-06-26 10:57:37 -0400

    implement rate knob

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # embedded-lab
 A collection of embedded systems experiments across Arduino, Raspberry Pi Pico, and STM32 platforms. Includes low-level firmware, state machines, and hardware interaction prototypes.
+
+See [contents](contends.md) for more details.
--- audio-experiments/07-step-sequencer/07-step-sequencer.ino
+++ audio-experiments/07-step-sequencer/07-step-sequencer.ino
@@ -4,8 +4,13 @@
 
 
 #define OUTPUT_PIN 8
+#define RATE_POT_PIN A0
 
-unsigned long STEP_LENGTH_MS = 500;
+#define DELAY_READ_RATE_KNOB 50
+
+unsigned long STEP_LENGTH_RANGE_MS[2] = {10, 1000};
+unsigned long step_length_ms;
+unsigned long time_last_rate_knob_read_ms;
 
 // step values (0-256)
 uint8_t step_vals[5] = {50, 100, 150, 200, 250};
@@ -14,12 +19,23 @@ unsigned long time_last_step_ms;
 uint8_t current_step_idx;
 bool step_event;
 
+int read_rate_knob() {
+    // Read raw value
+    int raw = analogRead(RATE_POT_PIN); // 0 - 1023
+
+    // Scale to the range
+    return STEP_LENGTH_RANGE_MS[0] + (int)(raw * (STEP_LENGTH_RANGE_MS[1] - STEP_LENGTH_RANGE_MS[0]) / 1023.0);
+}
 
 void setup() {
     pinMode(OUTPUT_PIN, OUTPUT);
+    pinMode(RATE_POT_PIN, INPUT);
+
     current_step_idx = 0;
     step_event = false;
     time_last_step_ms = millis();
+    time_last_rate_knob_read_ms = millis();
+    step_length_ms = read_rate_knob();
 
     Serial.begin(115200);
 }
@@ -28,7 +44,7 @@ void loop() {
     unsigned long now = millis();
 
     // detect if a step change has occurred
-    if (now - time_last_step_ms > STEP_LENGTH_MS) {
+    if (now - time_last_step_ms > step_length_ms) {
         step_event = true;
         time_last_step_ms = now;
     }
@@ -43,5 +59,11 @@ void loop() {
         analogWrite(OUTPUT_PIN, output_val);
         Serial.print("Output val has changed to ");
         Serial.println(output_val);
+    }
+
+    // If it's time to sample the rate pot, do so
+    if (now - time_last_rate_knob_read_ms > DELAY_READ_RATE_KNOB) {
+        int new_rate = read_rate_knob();
+        step_length_ms = new_rate;
     }
 }
