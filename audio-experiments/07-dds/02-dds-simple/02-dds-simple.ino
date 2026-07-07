2dfb128d6a60b914bceb731568d9c44ad6856ed1
StanleyTuz <stanley.tuznik@gmail.com>
2026-07-07 14:14:43 -0400

    simple dds example

---
+++ audio-experiments/07-dds/02-dds-simple/02-dds-simple.ino
@@ -0,0 +1,86 @@
+/* Output a 440 Hz sine wave via DAC. */
+
+#include <Arduino.h>
+#include "sine.h"
+#include "triangle.h"
+
+#define F_OUT 440
+#define F_SAMPLE 20000
+#define PORT_OUT PORTA
+#define DDR_OUT DDRA
+
+volatile bool do_sample = false;
+uint16_t phase_accumulator = 0;
+uint16_t phase_increment = 1442;
+uint8_t phase_index;
+uint8_t value;
+
+/* Configure the 16-bit Timer1.
+
+    To sample rate F_SAMPLE, we need to interrupt every
+    F_CPU / F_SAMPLE ticks.
+*/
+void configure_timer1() {
+
+    TCCR1A = 0;
+    TCCR1B = 0;
+    
+    // Set WGM to CTC
+    TCCR1B |= (1 << WGM12);
+    // Keep Output Mode normal (not using output pin)
+
+    // Set the prescaler to 1
+    TCCR1B |= (1 << CS10);
+
+    // Set the overflow compare register
+    OCR1A = 800; // F_CPU / F_SAMPLE;
+
+    // Enable the interrupts on this timer
+    TIMSK1 |= (1 << OCIE1A);
+
+    sei();
+}
+
+ISR(TIMER1_COMPA_vect) {
+    do_sample = true;
+}
+
+void setup() {
+    // Serial.begin(115200);
+
+    // Set output pins to output mode
+    DDR_OUT = 0xFFFF;
+
+    configure_timer1();
+
+    phase_accumulator = 0;
+}
+
+
+void sample_table() {
+    // Take a sample
+    phase_accumulator += phase_increment;
+    // Shift to get the index
+    phase_index = (phase_accumulator >> 8);
+    // Index into the LUT
+    value = 128 + fullTriangle[phase_index];
+}
+
+void output_to_dac() {
+    // Write the output value to the port
+    PORT_OUT = value;
+}
+
+void loop() {
+    if (do_sample) {
+        do_sample = false;
+
+        // Take a sample
+        sample_table();
+        
+        // Output the current values
+        output_to_dac();
+
+        // Serial.println(value);
+    }
+}
